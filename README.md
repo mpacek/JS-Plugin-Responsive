a2a72cc0ad16b97823901a2a4ad7b3d795e90314
Michał Pacek <m.pacek@onet.eu>
2015-12-20 16:07:49 +0100

    check if function exists added, readme change

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # Responsive v.1.0.0
 
 jQuery helper plugin, can be used to launch scripts on mobile, and desktop. As parameters takes breakpoint value, and functions to run.
+
+**How to use:**
+
+	var mobileFunction = function(),
+			desktopFunction = function();
+
+	if ($.fn.responsiveSetup) {
+	  $(document).responsiveSetup({
+	    runMobile: { callFunction: mobileFunction },
+	    runDesktop: { callFunction: desktopFunction }
+	  });
+	}
--- responsive.js
+++ responsive.js
@@ -1,35 +1,45 @@
 "use strict";
 
+// How to use:
+//
+// if ($.fn.responsiveSetup) {
+//   $(document).responsiveSetup({
+//     runMobile: { callFunction: mobileFunction },
+//     runDesktop: { callFunction: desktopFunction }
+//   });
+// }
+
 // Mobile setup
 $.fn.responsiveSetup = function (settings) {
   settings = jQuery.extend({
-    onMobile: '',
-    onDesktop: '',
+    runMobile: '',
+    runDesktop: '',
     breakpoint: 980
   }, settings);
 
-  var stateMobile = false;
+  var onMobile = false;
 
   // launch
   if ($(window)[0].innerWidth < settings.breakpoint) {
-    settings.onMobile.callFunction();
-    stateMobile = true;
+    settings.runMobile.callFunction();
+    onMobile = true;
   }
 
   // on window resize
   $(window).resize(function () {
     var windowSize = $(window)[0].innerWidth;
 
-    if ((windowSize >= settings.breakpoint) && stateMobile) {
-      settings.onDesktop.callFunction();
-      stateMobile = false;
+    if ((windowSize >= settings.breakpoint) && onMobile) {
+      if (typeof settings.runDesktop === "function") {
+        settings.runDesktop.callFunction();
+      }
+      onMobile = false;
     }
-    else if ((windowSize < settings.breakpoint) && !stateMobile) {
-      settings.onMobile.callFunction();
-      stateMobile = true;
-    }
-    else {
-      return;
+    else if ((windowSize < settings.breakpoint) && !onMobile) {
+      if (typeof settings.runMobile === "function") {
+        settings.runMobile.callFunction();
+      }
+      onMobile = true;
     }
   });
 }
